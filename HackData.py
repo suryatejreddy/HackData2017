fd4888ed26d440dfd0ba56afb1e6130d51c07b43
Suryatej Reddy Vyalla <suryatej16102@iiitd.ac.in>
2017-10-14 17:33:59 +0400

    updated flask code

--- HackData.py
+++ HackData.py
@@ -7,25 +7,6 @@ import json
 
 app = Flask(__name__)
 
-# d = dict()
-# data = pd.read_csv("buildings.csv")
-# L = []
-# maxim = 0
-# maxim_data = []
-# for index, row in data.iterrows():
-#     d = dict()
-#     data = row.tolist()
-#     coordinates = ast.literal_eval(data[0])
-#     d['lat'] = coordinates[0]
-#     d['lng'] = coordinates[1]
-#     d['count'] = len(ast.literal_eval(data[-2]))
-#     if d['count'] > maxim :
-#         maxim = d['count']
-#         maxim_data = ast.literal_eval(data[-2])
-#     L.append(d)
-# f = open('data.dat', 'wb')
-# pickle.dump((L, maxim), f)
-
 f = open('dataFinal.dat','rb')
 t = pickle.load(f)
 L = t[0]
